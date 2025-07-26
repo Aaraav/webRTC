2eea4f6c8fb99fc4def721589da19bda5be0c533
Aaraav <76102028+Aaraav@users.noreply.github.com>
2025-07-26 13:45:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,106 +3,94 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
 const io = new Server(server, {
   cors: {
     origin: "*"
   }
 });
 
-const rooms = {};
 const users = {};
+const rooms = {};
 
 io.on('connection', (socket) => {
-  console.log('a user connected ' + socket.id);
-
-  socket.on("disconnect", (params) => {
-    Object.keys(rooms).map(roomId => {
-      rooms[roomId].users = rooms[roomId].users.filter(x => x !== socket.id)
-    })
-    delete users[socket.id];
-  })
+  console.log('A user connected: ' + socket.id);
 
   socket.on("join", (params) => {
     const roomId = params.roomId;
+    socket.join(roomId);
 
-        socket.join(roomId);
+    users[socket.id] = { roomId };
 
-    users[socket.id] = {
-      roomId: roomId
-    }
     if (!rooms[roomId]) {
-      rooms[roomId] = {
-        roomId,
-        users: []
-      }
+      rooms[roomId] = { users: [] };
     }
+
+    const otherUsers = rooms[roomId].users;
+    if (otherUsers.length > 0) {
+      io.to(socket.id).emit("other-users", otherUsers);
+    }
+
     rooms[roomId].users.push(socket.id);
-    console.log("user added to room " + roomId);
+    console.log(`User ${socket.id} added to room ${roomId}`);
   });
 
-  socket.on("localDescription", (params) => {
-    let roomId = users[socket.id].roomId;
-    
-    let otherUsers = rooms[roomId].users;
-    otherUsers.forEach(otherUser => {
-      if (otherUser !== socket.id) {
-        io.to(otherUser).emit("localDescription", {
-            description: params.description
-        })
+  socket.on("disconnect", () => {
+    console.log(`A user disconnected: ${socket.id}`);
+    const user = users[socket.id];
+
+    if (user) {
+      const roomId = user.roomId;
+      const room = rooms[roomId];
+
+      if (room) {
+        room.users = room.users.filter(id => id !== socket.id);
+
+        if (room.users.length === 0) {
+          delete rooms[roomId];
+          console.log(`Room ${roomId} is empty and has been closed.`);
+        } else {
+          socket.to(roomId).emit("peer-left", { socketId: socket.id });
+        }
       }
-    })
-  })
+      delete users[socket.id];
+    }
+  });
+
+  const broadcastToRoom = (eventName, params) => {
+    const user = users[socket.id];
+    if (user) {
+      const roomId = user.roomId;
+      socket.to(roomId).emit(eventName, params);
+    } else {
+      console.log(`Warning: User ${socket.id} tried to emit '${eventName}' but was not in a room.`);
+    }
+  };
+
+  socket.on("localDescription", (params) => {
+    broadcastToRoom("localDescription", { description: params.description });
+  });
 
   socket.on("remoteDescription", (params) => {
-    let roomId = users[socket.id].roomId;    
-    let otherUsers = rooms[roomId].users;
-
-    otherUsers.forEach(otherUser => {
-      if (otherUser !== socket.id) {
-        io.to(otherUser).emit("remoteDescription", {
-            description: params.description
-        })
-      }
-    })
+    broadcastToRoom("remoteDescription", { description: params.description });
   });
 
   socket.on("iceCandidate", (params) => {
-    let roomId = users[socket.id].roomId;    
-    let otherUsers = rooms[roomId].users;
-
-    otherUsers.forEach(otherUser => {
-      if (otherUser !== socket.id) {
-        io.to(otherUser).emit("iceCandidate", {
-          candidate: params.candidate
-        })
-      }
-    })
+    broadcastToRoom("iceCandidate", { candidate: params.candidate });
   });
-
 
   socket.on("iceCandidateReply", (params) => {
-    let roomId = users[socket.id].roomId;    
-    let otherUsers = rooms[roomId].users;
-
-    otherUsers.forEach(otherUser => {
-      if (otherUser !== socket.id) {
-        io.to(otherUser).emit("iceCandidateReply", {
-          candidate: params.candidate
-        })
-      }
-    })
+    broadcastToRoom("iceCandidateReply", { candidate: params.candidate });
   });
-  socket.on('chat-message', (params) => {
-    const { text, roomId, author } = params;
-    // Broadcast the message to all other users in the room
-    socket.to(roomId).emit('chat-message', {
-      text,
-      author, // Forward the author's name
-    });
-  });
 
+  socket.on('chat-message', (params) => {
+    broadcastToRoom('chat-message', {
+      text: params.text,
+      author: params.author,
+    });
+  });
 });
 
 server.listen(3001, () => {
-  console.log('listening on *:3001');
+  console.log('Server is listening on *:3001');
 });
